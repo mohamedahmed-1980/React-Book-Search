b768e3600cdf3643233e942be8b80aec5fcd4c7d
mohamedahmed-1980 <mohamedahmed0912@hotmail.com>
2021-03-23 22:26:34 -0400

    back end

---
+++ routes/api/books.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/saved")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Matches with "/api/books/:id"
+router
+  .route("/saved/:id")
+  .get(booksController.findById)
+  .delete(booksController.remove);
+
+module.exports = router;
